6edbd1f4ac0cc23524e7d2e987801f9f69ef0262
Tremain Hebert <truman@Tremains-Mac-mini.local>
2020-08-29 19:55:38 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 // Components
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
@@ -19,16 +20,18 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
